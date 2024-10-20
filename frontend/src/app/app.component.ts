4d8f945cb48d652707d50b4fae364fff4c5a798e
Aguila13 <jpierre.11@hotmail.com>
2024-10-19 23:38:34 -0500

    feat: implements recompensas table

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecompensaComponent } from './components/recompensa/recompensa.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RecompensaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
