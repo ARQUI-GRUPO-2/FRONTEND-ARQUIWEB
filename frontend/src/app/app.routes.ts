258235c868d7ec7405b5e0fd929a7ce6f18d3338
Aguila13 <jpierre.11@hotmail.com>
2024-10-27 23:32:21 -0500

    feat: added CRUD in recompensas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { RecompensaComponent } from './components/recompensa/recompensa.component';
+import { CreareditarrecompensaComponent } from './components/recompensa/creareditarrecompensa/creareditarrecompensa.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'recompensas',component:RecompensaComponent,
+        children:[
+            {
+                path:'nuevo',component:CreareditarrecompensaComponent
+            },
+            {
+              path:'ediciones/:id',component:CreareditarrecompensaComponent
+            }
+        ]
+    }
+];
