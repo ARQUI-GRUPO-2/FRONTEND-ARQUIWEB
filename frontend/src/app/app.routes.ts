a1d8d68f7b88ed58d898e6f304de41b163f32d30
M1guelVF <mvfernandez1492@gmail.com>
2024-10-27 19:42:37 -0500

    feat: implements creareditaractividad table

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ActividadComponent } from './components/actividad/actividad.component';
+import { CreaeditaactividadComponent } from './components/actividad/creaeditaactividad/creaeditaactividad.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'actividades',component:ActividadComponent,
+        children:[{
+            path:'nuevo',component:CreaeditaactividadComponent
+        }]
+    }
+];
--- frontend/src/app/components/actividad/listaractividad/listaractividad.component.ts
+++ frontend/src/app/components/actividad/listaractividad/listaractividad.component.ts
@@ -2,23 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Actividad } from '../../../models/Actividad';
 import { ActividadService } from '../../../services/actividad.service';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listaractividad',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatIconModule, RouterLink],
   templateUrl: './listaractividad.component.html',
   styleUrl: './listaractividad.component.css'
 })
 export class ListaractividadComponent implements OnInit {
   dataSource:MatTableDataSource<Actividad>= new MatTableDataSource()
 
-  displayedColumns:string[]=['c1','c2','c3','c4','c5','c6','c7','c8','c9','c10']
+  displayedColumns:string[]=['c1','c2','c3','c4','c5','c6','c7','c8','c9','c10','accion01','accion02']
   constructor(private aS: ActividadService) { }
   
   ngOnInit(): void {
     this.aS.list().subscribe(data => {
       this.dataSource = new MatTableDataSource(data);
+    });
+    this.aS.getList().subscribe(data=>{
+      this.dataSource=new MatTableDataSource(data)
+    });
+  }
+
+  eliminar(id:number){
+    this.aS.delete(id).subscribe((data)=>{
+      this.aS.list().subscribe((data)=>{
+        this.aS.setList(data);
+      });
     });
   }
 
