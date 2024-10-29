3960d4ffc03a2fec3cd8f6d8074e15bd89d26f38
Aguila13 <jpierre.11@hotmail.com>
2024-10-29 00:20:01 -0500

    feat: added paginator

--- frontend/src/app/components/noticias/listarnoticias/listarnoticias.component.ts
+++ frontend/src/app/components/noticias/listarnoticias/listarnoticias.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Noticias } from '../../../models/Noticias';
 import { NoticiasService } from '../../../services/noticias.service';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 
 @Component({
   selector: 'app-listarnoticias',
   standalone: true,
-  imports: [MatTableModule, MatIconModule, RouterLink],
+  imports: [MatTableModule, MatIconModule, RouterLink, MatPaginatorModule],
   templateUrl: './listarnoticias.component.html',
   styleUrl: './listarnoticias.component.css'
 })
@@ -19,6 +20,8 @@ export class ListarnoticiasComponent implements OnInit {
 
   displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'accion01','accion02'];
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(private nS: NoticiasService) { }
 
   ngOnInit(): void {
@@ -27,13 +30,19 @@ export class ListarnoticiasComponent implements OnInit {
     });
     this.nS.getList().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
     });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
   eliminar(id: number) {
     this.nS.delete(id).subscribe((data) => {
       this.nS.list().subscribe((data) => {
         this.nS.setList(data);
+        this.dataSource.paginator = this.paginator;
       });
     });
   }
