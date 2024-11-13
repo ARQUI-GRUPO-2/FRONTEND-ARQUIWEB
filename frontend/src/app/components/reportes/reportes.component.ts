e762e62576f8f554d58a48767ba8916a3a8b8590
Heransue Geldres <149516843+hersite@users.noreply.github.com>
2024-11-12 19:53:39 -0500

    feat: implement reports and logout security

--- frontend/src/app/components/reportes/reportes.component.ts
+++ frontend/src/app/components/reportes/reportes.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { ActividadesporcentroComponent } from './actividadesporcentro/actividadesporcentro.component';
+import { CentrosfavoritosComponent } from './centrosfavoritos/centrosfavoritos.component';
+import { CentrosusuariosComponent } from './centrosusuarios/centrosusuarios.component';
 
 @Component({
   selector: 'app-reportes',
   standalone: true,
-  imports: [RouterOutlet, ActividadesporcentroComponent],
+  imports: [RouterOutlet, ActividadesporcentroComponent, CentrosfavoritosComponent, CentrosusuariosComponent],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.css'
 })
